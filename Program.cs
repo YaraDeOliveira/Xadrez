90b238cee88930b86eb39f3be81126b664330740
Yara <yara.regina@hotmail.com>
2021-12-24 16:06:44 -0300

    ajsute inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+            Console.ReadLine();
+        }
+    }
+}
