5a358712b9340d2cc4bc5c5207fb490e6b42d757
Yara <yara.regina@hotmail.com>
2021-12-28 20:32:26 -0300

    Implementando as restrições de movimento

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -7,7 +7,7 @@
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca (Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
             this.tab = tab;
@@ -15,12 +15,33 @@
             this.qteMovimentos = 0;
         }
 
-        public void incrementarQteMovimentos() 
+        public void incrementarQteMovimentos()
         {
             qteMovimentos++;
         }
 
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.linhas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
         public abstract bool[,] movimentosPossiveis();
-     
+
     }
 }
